762e1b0176312ca2be58b6709242692d36631211
Janvi Madhok <147023281+janvi-madhok@users.noreply.github.com>
2024-02-10 20:17:32 +0530

    Add files via upload

---
+++ ASCIIValue.java
@@ -0,0 +1,17 @@
+public class ASCIIValue{
+
+    public static void main(String args[]){
+
+        char ch = 'B';
+        int ASCII = 'd';
+        int a = 69;
+        char b = 88;
+        
+        System.out.println((int)ch);
+        System.out.println(ASCII);
+        System.out.println((char)a);
+        System.out.println((char)b);
+
+
+    }
+}
