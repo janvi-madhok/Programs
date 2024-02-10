c5c117d3d1c8eb6c1d76df49a58d50da8ec10c64
Janvi Madhok <147023281+janvi-madhok@users.noreply.github.com>
2024-02-10 20:12:09 +0530

    Add files via upload

---
+++ RandomNumber.java
@@ -0,0 +1,11 @@
+public class RandomNumber{
+
+    public static void main(String args[]){
+
+        // Dice random number : 1 to 6, OTP - random 4 digit number
+
+        int random = (int)(Math.random()*10);
+        System.out.println(random);
+
+    }
+}
