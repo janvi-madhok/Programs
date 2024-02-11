ac35d305ff7a87c70794783207c428ac39ac89aa
Janvi Madhok <147023281+janvi-madhok@users.noreply.github.com>
2024-02-11 14:28:47 +0530

    Add files via upload

---
+++ SquareRootOfNumber.java
@@ -0,0 +1,12 @@
+public class SquareRootOfNumber{
+
+    public static void main(String args[]){
+
+        int number = 9;
+        // 10^2 = 10*10 = 100
+        // 7^2 = 7*7 = 49
+
+        System.out.println("Sqrt of Number "+number+" : "+(number*number));
+
+    }
+}
