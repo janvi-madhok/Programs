51475564660888b6ff6b767eee1a962b04f11206
Janvi Madhok <147023281+janvi-madhok@users.noreply.github.com>
2024-02-13 22:39:28 +0530

    Update CopyArrayElementsInAnotherArray.java

--- CopyArrayElementsInAnotherArray.java
+++ CopyArrayElementsInAnotherArray.java
@@ -3,7 +3,7 @@ public class CopyArrayElementsInAnotherArray {
     public static void main(String[] args) {
         
         int arr1[] = {2,1,6,4,8};
-        int arr2[] = new int[5];
+        int arr2[] = new int[arr1.length];
 
         for(int i=0; i<arr1.length; i++){
             arr2[i] = arr1[i];
