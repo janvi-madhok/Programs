8a28488859f38b199712d4ce60f5ee0ad52584c3
Janvi Madhok <147023281+janvi-madhok@users.noreply.github.com>
2024-02-13 22:37:43 +0530

    Add files via upload

---
+++ CopyArrayElementsInAnotherArray.java
@@ -0,0 +1,25 @@
+public class CopyArrayElementsInAnotherArray {
+    
+    public static void main(String[] args) {
+        
+        int arr1[] = {2,1,6,4,8};
+        int arr2[] = new int[5];
+
+        for(int i=0; i<arr1.length; i++){
+            arr2[i] = arr1[i];
+        }
+
+        System.out.print("Array 1 Elements : ");
+
+        for(int i=0; i<arr1.length; i++){
+            System.out.print(arr1[i]+" ");
+        }
+
+        System.out.print("\nArray 2 Elements : ");
+
+        for(int i=0; i<arr2.length; i++){
+            System.out.print(arr2[i]+" ");
+        }
+
+    }
+}
