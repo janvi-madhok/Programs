2968fb7248490559917b2d91af78db2b2a3b1d22
Janvi Madhok <147023281+janvi-madhok@users.noreply.github.com>
2024-02-13 12:47:21 +0530

    Add files via upload

---
+++ PositiveOrNegativeNumber.java
@@ -0,0 +1,32 @@
+public class PositiveOrNegativeNumber {
+    
+    public static void main(String[] args) {
+        
+        int num1 = 12;
+        int num2 = -67;
+        int num3 = 0;
+
+        if(num1>0)
+        System.out.println(num1+" is Positive Number");
+        else if(num1==0)
+        System.out.println(num1+" is Zero");
+        else
+        System.out.println(num1+" is Negative Number");
+
+        if(num2>0)
+        System.out.println(num2+" is Positive Number");
+        else if(num1==0)
+        System.out.println(num1+" is Zero");
+        else
+        System.out.println(num2+" is Negative Number");
+
+        if(num3>0)
+        System.out.println(num3+" is Positive Number");
+        else if(num3==0)
+        System.out.println(num3+" is Zero");
+        else
+        System.out.println(num3+" is Negative Number");
+
+
+    }
+}
