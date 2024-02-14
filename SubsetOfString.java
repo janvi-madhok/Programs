efee364cf741010fd10ab1512351fbe1c3ca2dff
Janvi Madhok <147023281+janvi-madhok@users.noreply.github.com>
2024-02-14 20:56:33 +0530

    Add files via upload

---
+++ SubsetOfString.java
@@ -0,0 +1,32 @@
+public class SubsetOfString {
+    
+    public static void main(String[] args) {
+        
+        // ABC - A B C AB BC ABC
+
+        String str = "FUNNY";
+        int len = str.length();
+        int temp=0;
+        int substringLen = (len*(len+1))/2;
+
+        String arr[] = new String[substringLen];
+
+        for(int i=0;i<len;i++){
+
+            for(int j=i;j<len;j++){
+
+                arr[temp]=str.substring(i, j+1);
+                temp++;
+
+            }
+        }
+
+        for(int i=0;i<substringLen;i++){
+
+            System.out.print(arr[i]+" ");
+
+        }
+
+
+    }
+}
